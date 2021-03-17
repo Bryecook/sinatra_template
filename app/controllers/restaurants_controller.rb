6f8faf5f18abd46dc16e59e92c2c82e070f19533
Bryecook <brybcook@gmail.com>
2021-03-17 15:49:23 -0700

    final

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,22 @@
 class RestaurantsController < ApplicationController
     set :method_override, true
-    
+    set :views, "app/views/restaurants", "app/views/cities"
 
+    get '/restaurants' do
+        @restaurants=Restaurant.all
+        erb :index
+    end
+
+    get '/restaurants/new' do
+        @cities=City.all
+        erb :new
+    end
+
+    post '/restaurants' do
+        id= params["city_id"].to_i
+        @restaurant = Restaurant.create(name: params["restaurant_name"], city_id: params["city_id"].to_i)
+        redirect '/restaurants'
+        # redirect '/cities/:#{@restaurant.city_id}'
+    end 
 
 end
