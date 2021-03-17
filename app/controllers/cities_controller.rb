5ddad323f7dda344b47969556878bacc68167c36
RobertObi <robobi97@googlemail.com>
2021-03-17 15:13:45 -0500

    3/4

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -13,5 +13,39 @@ class CitiesController < ApplicationController
         erb :show
     end
 
+    get '/create_city' do
+        erb :create_city
+    end 
+
+    post '/cities' do
+        @city = City.create(name: params["city_name"], state: params["city_state"])
+        redirect "/cities/#{@city.id}"
+    end
+
+    get '/cities/:id/edit' do
+        @city = City.find(params[:id])
+
+        erb :edit
+
+    end
+
+
+    patch '/cities/:id' do
+        @city = City.find(params[:id])
+        @city.update(name: params["city_name"], state: params["city_state"])
+
+        redirect "/cities/#{@city.id}"
+    end
+
+
+    delete '/cities/:id' do 
+       @city = City.find(params[:id])
+       @city.destroy
+
+       redirect '/cities'
+    end
+
+
+
     
 end
